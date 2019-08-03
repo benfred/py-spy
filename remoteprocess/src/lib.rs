dd4bfe4d72e5e5e3c7e56e3dc6dd682a8faec136
Ben Frederickson <github@benfrederickson.com>
2019-08-02 20:20:18 -0700

    Fix thread activity for i686/ARM/FreeBSD
    The heuristic thread activity fallback was being called incorrectly, leading
    to active threads being marked as idle.
    
    Fix, and add a Rust integration test that would have caught this

--- remoteprocess/src/lib.rs
+++ remoteprocess/src/lib.rs
@@ -155,7 +155,7 @@ impl std::error::Error for Error {
         }
     }
 
-    fn cause(&self) -> Option<&std::error::Error> {
+    fn cause(&self) -> Option<&dyn std::error::Error> {
         match *self {
             Error::GimliError(ref e) => Some(e),
             Error::GoblinError(ref e) => Some(e),
