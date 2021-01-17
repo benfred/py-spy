bd03a6bad9d66b5408885678391879dcf5d227eb
Ben Frederickson <github@benfrederickson.com>
2021-01-16 23:51:58 -0800

    Upload release wheels (#348)
    Create wheels as part of the github actions, and on a tagged release upload these wheels to pypi/github releases page. 
    Creates x86_64 wheels for osx/linux/mac and also i686/armv7/aarch64 wheels for linux.

--- ci/install_rust.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d ~/rust-installer ]; then 
-    mkdir ~/rust-installer
-    curl -sL https://static.rust-lang.org/rustup.sh -o ~/rust-installer/rustup.sh
-    sh ~/rust-installer/rustup.sh --prefix=~/rust --spec=stable --disable-sudo -y 
-fi
--- ci/ubuntu64/build_wheels.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd /code/py-spy
-export PYSPY_CROSS_COMPILE_TARGET=x86_64-unknown-linux-musl
-python3 setup.py bdist_wheel
