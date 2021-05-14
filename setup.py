a9f4c9cd2c01a73fb9298b4be8df731047798fed
Adam Johnson <me@adamj.eu>
2021-05-14 05:23:46 +0100

    Change PyPI dev status to "Production/Stable" (#394)
    py-spy works well, so mark it as stable.

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
     license="MIT",
     cmdclass={"install": PostInstallCommand, "bdist_wheel": bdist_wheel},
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
         "Intended Audience :: Developers",
