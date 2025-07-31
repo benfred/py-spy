9b8d388b3965fcc9fc9e4c1ead56778d792cc1b2
Ben Frederickson <github@benfrederickson.com>
2025-07-30 21:13:25 -0700

    cargo fmt / update CI to enable unwind on arm/aarch64

--- build.rs
+++ build.rs
@@ -3,10 +3,10 @@ use std::env;
 fn main() {
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    match (target_arch.as_ref(), target_os.as_ref())  {
-        ("x86_64", "windows") |
-        ("x86_64", "linux") |
-        ("arm", "linux")  =>  println!("cargo:rustc-cfg=unwind"),
-        _ => { }
+    match (target_arch.as_ref(), target_os.as_ref()) {
+        ("x86_64", "windows") | ("x86_64", "linux") | ("arm", "linux") => {
+            println!("cargo:rustc-cfg=unwind")
+        }
+        _ => {}
     }
 }
