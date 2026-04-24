13c8863569cdd220c3d1a611f7c87ee515a845a9
Till Ehrengruber <t.ehrengruber@me.com>
2026-04-24 23:17:51 +0200

    Add support for native extensions on linux aarch64  (#779)

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     match (target_arch.as_ref(), target_os.as_ref()) {
-        ("x86_64", "windows") | ("x86_64", "linux") | ("arm", "linux") => {
+        ("x86_64", "windows") | ("x86_64", "linux") | ("arm", "linux") | ("aarch64", "linux") => {
             println!("cargo:rustc-cfg=unwind")
         }
         _ => {}
